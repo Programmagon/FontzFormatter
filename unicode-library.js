53d1ec761ca1252a14ded4b8ad2d6dd96f21b1d6
Poisiniously <vbozjak@Icloud.com>
2026-07-15 11:43:31 +0200

    Update UnicodeLibrary with new styles and functions

--- unicode-library.js
+++ unicode-library.js
@@ -1,11 +1,10 @@
 /**
- * CYBER_FONT // EXPANDED UNICODE LIBRARY
+ * CYBER_FONT // ULTIMATE UNICODE LIBRARY (COMPLETE VERSION)
  * Core mapping protocol for multi-layered text translation.
  */
 const UnicodeLibrary = (() => {
   
   // 1. MATHEMATICAL UNICODE BLOCKS (Calculated via code point offsets)
-  // Format: { A: [Uppercase Hex], a: [Lowercase Hex], 0: [Digit Hex], exceptions: { char: 'replacement' } }
   const offsetMaps = {
     bold:           { A: 0x1D400, a: 0x1D41A, 0: 0x1D7CE },
     italic:         { A: 0x1D434, a: 0x1D44E, 0: 0x00030, exceptions: { h: 'ℎ' } },
@@ -24,40 +23,43 @@ const UnicodeLibrary = (() => {
 
   // 2. CUSTOM DICTIONARY MAPS (Pseudo-fonts & special blocks)
   const charMaps = {
-    // V a p o r w a v e (Fullwidth)
     vaporwave: "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ０１２３４５６７８９",
-    // 🅂🅀🅄🄰🅁🄴🄳
     squared: "🄰🄱🄲🄳🄴🄵🄶🄷🄸🄹🄺🄻🄼🄽🄾🄿🅀🅁🅂🅃🅄🅅🅆🅇🅈🅉🄰🄱🄲🄳🄴🄵🄶🄷🄸🄹🄺🄻🄼🄽🄾🄿🅀🅁🅂🅃🅄🅅🅆🅇🅈🅉0123456789",
-    // 🅒🅘🅡🅒🅛🅔🅓 (Negative)
+    squaredDark: "🅰🅱🅲🅳🅴🅵🅶🅷🅸🅹🅺🅻🅼🅽🅾🅿🆀🆁🆂🆃🆄🆅🆆🆇🆈🆉🅰🅱🅲🅳🅴🅵🅶🅷🅸🅹🅺🅻🅼🅽🅾🅿🆀🆁🆂🆃🆄🆅🆆🆇🆈🆉０１２３４５６７８９",
     circledDark: "🅐🅑🅒🅓🅔🅕🅖🅗🅘🅙🅚🅛🅜🅝🅞🅟🅠🅡🅢🅣🅤🅥🅦🅧🅨🅩🅐🅑🅒🅓🅔🅕🅖🅗🅘🅙🅚🅛🅜🅝🅞🅟🅠🅡🅢🅣🅤🅥🅦🅧🅨🅩⓿❶❷❸❹❺❻❼❽❾",
-    // Ⓒⓘⓡⓒⓛⓔⓓ (Light)
     circledLight: "ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ⓪①②③④⑤⑥⑦⑧⑨",
-    // sᴍᴀʟʟ ᴄᴀᴘs
+    parenthesized: "⒜⒝⒞⒟⒠⒡⒢⒣⒤⒥⒦⒧⒨⒩⒪⒫⒬⒭⒮⒯⒰⒱⒲⒳⒴⒵⒜⒝⒞⒟⒠⒡⒢⒣⒤⒥⒦⒧⒨⒩⒪⒫⒬⒭⒮⒯⒰⒱⒲⒳⒴⒵０１２３４５６７８９",
     smallCaps: "ABCDEFGHIJKLMNOPQRSTUVWXYZᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ0123456789",
-    // uʍopǝpısdn (Upside Down)
     upsideDown: "∀𐐒ƆᗡƎℲ⅁HIſʞ˥WNOԀΌᴚS⊥∩ΛMX⅄Zɐqɔpǝɟƃɥıɾʞlɯuodbɹsʇnʌʍxʎz0ƖᄅƐㄣϛ9ㄥ86",
-    // Neo-Tokyo (Faux Asian / Kanji-Style)
-    fauxAsian: "卂乃匚刀巨下厶卄工丁长乚爪几口尸Ｑ尺丂丅凵Ｖ山乂丫乙卂乃匚刀巨下厶卄工丁长乚爪几口尸ｑ尺丂丅凵ｖ山乂丫乙０１２３４５６７８９"
+    fauxAsian: "卂乃匚刀巨下厶卄工丁长乚爪几口尸Ｑ尺丂丅凵Ｖ山乂丫乙卂乃匚刀巨下厶卄工丁长乚爪几口尸ｑ尺丂丅凵ｖ山乂丫乙０１２３４５６７８９",
+    fauxCyrillic: "ДБCДЄFGHИJКLMNФPQRSTЦVWXYZдбcдєfghиjкlmnфpqrstцvwxyz０１２３４５６７８９",
+    runic: "ᚫᛒᚳᛞᛖᚠᚷᚻᛁᛃᚲᛚᛗᚾᚩᛈᛩᚱᛋᛏᚢᚡᚹᛪᚣᛉᚫᛒᚳᛞᛖᚠᚷᚻᛁᛃᚲᛚᛗᚾᚩᛈᛩᚱᛋᛏᚢᚡᚹᛪᚣᛉ０１２３４５６７８９",
+    currency: "₳฿₵Đ€₣₲ⱧłJ₭Ⱡ₥₦Ø₱QⱤ₴₮ɄV₩ӾҰZ₳฿₵đ€₣₲Ⱨłj₭Ⱡ₥₦ø₱qⱤ₴₮ʉv₩ӿ¥z０１２３４５６７８９",
+    curly: "ǟɮƈɖɛʄɢɦɨʝӄʟʍռօքզʀֆȶʊʋաӼʏʐǟɮƈɖɛʄɢɦɨʝӄʟʍռօքզʀֆȶʊʋաӼʏʐ０１２３４５６７８９",
+    demonic: "ค๒૮๔ﻉिﻭɦٱﻝᛕɭ๓กѻρ۹ɼรՇપ۷ฝซץչค๒૮๔ﻉिﻭɦٱﻝᛕɭ๓กѻρ۹ɼรՇપ۷ฝซץչ０１２３４５６７８９",
+    leetspeak: "48CD3F6H1JKLMN0PQR57UVWXYZ48cd3f6h1jklmn0pqr57uvwxyz0123456789",
+    superscript: "ᴬᴮᶜᴰᴱᶠᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾQᴿˢᵀᵁⱽᵂˣʸᶻᵃᵇᶜᵈᵉᶠᵍʰᶦʲᵏˡᵐⁿᵒᵖᑫʳˢᵗᵘᵛʷˣʸᶻ⁰¹²³⁴⁵⁶⁷⁸⁹"
   };
 
   const standardChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  /**
-   * Applies the Glitch/Zalgo effect (Very Cyberpunk)
-   */
+  // --- Dynamic Modifier Functions (Zalgo, Strikethrough, Underline, Slash) ---
   function applyGlitch(text) {
     const glitchMarks = ['\u0300','\u0301','\u0302','\u0303','\u0304','\u0305','\u0306','\u0307','\u0308','\u0309','\u030A','\u030B','\u030C','\u030D','\u030E','\u030F','\u0310','\u0311','\u0312','\u0313','\u0314','\u0315','\u0316','\u0317','\u0318','\u0319','\u031A','\u031B','\u031C','\u031D','\u031E','\u031F'];
     return text.split('').map(char => {
       if (char === ' ') return char;
       const numMarks = Math.floor(Math.random() * 4);
       let glitched = char;
-      for (let i = 0; i < numMarks; i++) {
-        glitched += glitchMarks[Math.floor(Math.random() * glitchMarks.length)];
-      }
+      for (let i = 0; i < numMarks; i++) glitched += glitchMarks[Math.floor(Math.random() * glitchMarks.length)];
       return glitched;
     }).join('');
   }
 
+  function applyCombiningChar(text, combiningChar) {
+    return text.split('').map(char => char === ' ' ? char : char + combiningChar).join('');
+  }
+
+  // --- Core Translators ---
   function convertOffsetChar(char, map) {
     if (map.exceptions && map.exceptions[char]) return map.exceptions[char];
     const code = char.charCodeAt(0);
@@ -70,12 +72,41 @@ const UnicodeLibrary = (() => {
   function convertMappedChar(char, mapString) {
     const index = standardChars.indexOf(char);
     if (index !== -1) {
+        const array = Array.from(mapString); 
+        return array[index] || char;
+    }
+    return char;
+  }
+
+  // Main translation orchestrator
+  function translate(text, style) {
+    // Check dynamic modifiers first
+    if (style === 'glitch') return applyGlitch(text);
+    if (style === 'strikethrough') return applyCombiningChar(text, '\u0336');
+    if (style === 'underline') return applyCombiningChar(text, '\u0332');
+    if (style === 'slash') return applyCombiningChar(text, '\u0338');
+
+    // Check Math Offset Blocks
+    if (offsetMaps[style]) {
+      return text.split('').map(char => convertOffsetChar(char, offsetMaps[style])).join('');
+    }
+
+    // Check Custom Dictionary Blocks
+    if (charMaps[style]) {
+      let result = text.split('').map(char => convertMappedChar(char, charMaps[style])).join('');
+      if (style === 'upsideDown') result = result.split('').reverse().join('');
+      return result;
+    }
 
     return text;
   }
 
   return {
     translate,
-    getStyles: () => [...Object.keys(offsetMaps), ...Object.keys(charMaps), 'glitch']
+    getStyles: () => [
+      ...Object.keys(offsetMaps), 
+      ...Object.keys(charMaps), 
+      'glitch', 'strikethrough', 'underline', 'slash'
+    ]
   };
 })();
